0c26056145803c17749a9ab614767794bdf66df9
Ionut <ghitadumveanu@gmail.com>
2025-05-03 10:55:51 +0300

    ok

--- src/Routes/Search.tsx
+++ src/Routes/Search.tsx
@@ -4,6 +4,7 @@ import Footer from '../Components/Footer';
 import { useEffect, useState } from 'react';
 
 type ArticleProps = {
+  _id: string,
   title: string,
   content: string,
   category: string,
@@ -43,7 +44,7 @@ export default function Search() {
       <h1 className='text-center text-lg md:text-2xl font-semibold py-10 text-gray-600'>{error}</h1>
       <div className='flex justify-center mb-8'>
           <div className="grid gap-5" id='articlesDisplay'>
-            {articles?.map((article, index) => <Link to={`/article/${article.title}`} key={index}>
+            {articles?.map((article, index) => <Link to={`/article/${article._id}`} key={index}>
               <fieldset className="border-2 border-gray-400 w-full h-80 rounded-lg flex flex-col items-center">
                   <img src={article.imageUrl} alt="articleImage" className='w-full h-1/2' />
                   <h1 className="text-md font-semibold p-2">{article.title}</h1>
