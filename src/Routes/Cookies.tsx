4b86865ca9b7956edfcb0de7b873a00eaab1fb7b
Ionut <ghitadumveanu@gmail.com>
2025-05-04 09:35:54 +0300

    cookies

--- src/Routes/Cookies.tsx
+++ src/Routes/Cookies.tsx
@@ -14,11 +14,11 @@ export default function Cookies() {
             <p className="mb-4 text-lg py-2">
             Site-ul nostru folosește cookies pentru a îmbunătăți experiența utilizatorilor și pentru a asigura funcționarea corectă a anumitor funcționalități, în special în ceea ce privește autentificarea utilizatorilor.
             </p>
-            <h2>Ce sunt cookies?</h2>
+            <h2 className="text-xl">Ce sunt cookies?</h2>
             <p className="mb-4 text-lg">
                 Cookies sunt fișiere mici care sunt stocate pe dispozitivul dumneavoastră atunci când accesați site-ul nostru. Acestea ne ajută să păstrăm sesiunea de autentificare activă și să îmbunătățim performanța site-ului.
             </p>
-            <h2>Cum utilizăm cookies?</h2>
+            <h2 className="text-xl">Cum utilizăm cookies?</h2>
             <p>
                 Acestea sunt utilizate pentru a păstra sesiunea de autentificare activă, astfel încât să nu fie necesar să vă reconectați la fiecare accesare a site-ului.
             </p>
