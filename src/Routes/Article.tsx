138db46e45522f59740f88503ce7b64653ae56df
Ionut <ghitadumveanu@gmail.com>
2025-06-12 14:45:07 +0300

    of

--- src/Routes/Article.tsx
+++ src/Routes/Article.tsx
@@ -76,7 +76,7 @@ export default function Article() {
           </div>
           {isEditing ? 
           <>
-            <input type="text" className='bg-gray-100 border overflow-x-auto border-gray-500 py-2 px-1 rounded-lg w-full' placeholder='title' defaultValue={currentArticle?.title} />
+            <input type="text" className='bg-gray-100 border overflow-x-scroll border-gray-500 py-2 px-1 rounded-lg w-full' placeholder='title' defaultValue={currentArticle?.title} />
             <textarea className='bg-gray-100 border max-[800px]:h-60 border-gray-500 py-2 px-1 rounded-lg w-full overflow-y-auto' placeholder='content' defaultValue={currentArticle?.content}></textarea>
             <div className='flex gap-3'>
               <button type='button' onClick={() => setIsEditing(true)} className='bg-green-700 text-white rounded-lg mb-3 p-1'>Save</button>
