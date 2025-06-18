469854cc5e138a1f735dff4e08968307bfb751ce
Ionut <ghitadumveanu@gmail.com>
2025-06-18 15:49:31 +0300

    ok

--- src/Routes/Article.tsx
+++ src/Routes/Article.tsx
@@ -145,11 +145,14 @@ export default function Article() {
           : 
           <>
             <h1 className='text-3xl md:text-4xl'>{currentArticle?.title}</h1>
-            <p className='text-lg md:text-xl pb-20 leading-normal whitespace-pre-wrap'>{contentHalf1}</p>
             {currentArticle?.imageUrl2 ? (
-              <img src={currentArticle?.imageUrl2} width={300} height={300} alt="articleImage2"/>
+              <>
+                <p className='text-lg md:text-xl pb-20 leading-normal whitespace-pre-wrap'>{contentHalf1}</p>
+                <img src={currentArticle?.imageUrl2} width={300} height={300} alt="articleImage2"/>
+                <p className='text-lg md:text-xl pb-20 leading-normal whitespace-pre-wrap'>{contentHalf2}</p>
+              </>           
             ) : null}   
-            <p className='text-lg md:text-xl pb-20 leading-normal whitespace-pre-wrap'>{contentHalf2}</p>
+            <p className='text-lg md:text-xl pb-20 leading-normal whitespace-pre-wrap'>{currentArticle?.content}</p>
             <div className='flex gap-3'>
               {logContext.isAdmin && <button type='button' onClick={() => deleteArticle(currentArticle!)} className='bg-red-700 text-white rounded-lg mb-3 p-1'>Delete Article</button>}
               {logContext.isAdmin && <button type='button' onClick={() => {
