93b970206b6e10ce96ddca3acf47680a29fda587
Ionut <ghitadumveanu@gmail.com>
2025-06-26 12:06:03 +0300

    ok

--- src/Routes/General.tsx
+++ src/Routes/General.tsx
@@ -53,11 +53,11 @@ export default function General() {
                         <img src={articles[0].imageUrl} alt="articleImage" className='opacity-70 w-full h-[500px] object-cover border-2 border-gray-400 rounded-lg' />
                         <h1 className='p-10 text-3xl absolute inset-0 font-extrabold' style={{zIndex: 10}}>{articles[0].title}</h1>
                       </div>  
-                      <select onChange={(e) => setSort(e.target.value)}>
+                      {/* <select onChange={(e) => setSort(e.target.value)}>
                         <option value="newest" defaultChecked>Cele mai noi</option>
                         <option value="popular" defaultChecked>Cele mai populare</option>
                         <option value="oldest">Cele mai vechi</option>
-                      </select>                 
+                      </select>                  */}
                 </fieldset> 
               </Link>)}
             {articles?.map((article, index) => <Link to={`/article/${article._id}`} key={index}>
