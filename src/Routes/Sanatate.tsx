fa2043c5cc80f8a028f1462adf0a0c06eb6a81af
Ionut <ghitadumveanu@gmail.com>
2025-07-31 14:48:08 +0300

    ok

--- src/Routes/Sanatate.tsx
+++ src/Routes/Sanatate.tsx
@@ -50,7 +50,7 @@ export default function Sanatate() {
                 <fieldset className="flex flex-col items-center border-2 border-blue-600 rounded-lg">
                       <div className='relative w-full'>
                         <img src={articles[0].imageUrl} alt="articleImage" className='opacity-80 w-full h-[500px] object-cover border-2 border-gray-400 rounded-lg' />
-                        <h1 className='p-10 text-3xl absolute inset-0 font-extrabold' style={{zIndex: 10}}>{articles[0].title}</h1>
+                        <h1 className='p-10 text-3xl absolute bottom-0 font-extrabold text-white' style={{zIndex: 10, textShadow: "2px 2px 6px black"}}>{articles[0].title}</h1>
                       </div>                     
                 </fieldset> 
               </Link>)}
